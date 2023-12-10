c4bed05b5c0b2809689cd20b9171115554a7f1e5
Mathias Olesen <math09s0@stud.kea.dk>
2023-12-10 17:41:52 +0100

    anim

--- src/Three.jsx
+++ src/Three.jsx
@@ -1,35 +1,273 @@
 import * as THREE from 'three';
-
 import { useEffect, useRef } from "react";
 
-export default function MyThree() {
+function MyThree() {
   const refContainer = useRef(null);
-  useEffect(() => {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
-    refContainer.current.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
-  }, []);
+  let group;
+const particlesData = [];
+let camera, scene, renderer;
+let positions, colors;
+let particles;
+let pointCloud;
+let particlePositions;
+let linesMesh;
+let targetX = 0;
+let targetY = 0;
+
+const maxParticleCount = 100;
+let particleCount = 100;
+const r = 400;
+const rHalf = r / 2;
+
+const effectController = {
+  showLines: true,
+  minDistance: 150,
+  maxConnections: 10,
+};
+
+useEffect(() => {
+  init();
+  window.addEventListener("resize", onWindowResize);
+  window.addEventListener("mousemove", handleMouseMove);
+
+  refContainer.current.appendChild(renderer.domElement);
+
+  // Start the animation loop
+  animate();
+
+  // Don't call render() here
+
+  // Clean up event listeners when the component unmounts
+  return () => {
+    window.removeEventListener("resize", onWindowResize);
+    window.removeEventListener("mousemove", handleMouseMove);
+  };
+
+},);
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    20,
+    window.innerWidth / window.innerHeight,
+    1,
+    4000
+  );
+  camera.position.z = 1750;
+
+  scene = new THREE.Scene();
+
+  group = new THREE.Group();
+  scene.add(group);
+
+  const segments = maxParticleCount * maxParticleCount;
+
+  positions = new Float32Array(segments * 3);
+  colors = new Float32Array(segments * 3);
+
+  const pMaterial = new THREE.PointsMaterial({
+    color: 0xffffff,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    sizeAttenuation: true,
+  });
+
+  particles = new THREE.BufferGeometry();
+  particlePositions = new Float32Array(maxParticleCount * 3);
+
+  for (let i = 0; i < maxParticleCount; i++) {
+    const theta = Math.random() * Math.PI; // polar angle
+    const phi = Math.random() * 2 * Math.PI; // azimuthal angle
+    const r = Math.random() * rHalf;
+
+    const x = r * Math.sin(theta) * Math.cos(phi);
+    const y = r * Math.sin(theta) * Math.sin(phi);
+    const z = r * Math.cos(theta);
+
+    particlePositions[i * 2] = x;
+    particlePositions[i * 2 + 1] = y;
+    particlePositions[i * 2 + 2] = z;
+
+    // add it to the geometry
+    particlesData.push({
+      velocity: new THREE.Vector3(
+        -1 + Math.random() * 2,
+        -1 + Math.random() * 2,
+        -1 + Math.random() * 2
+      ),
+      numConnections: 0,
+    });
+  }
+
+  particles.setDrawRange(0, particleCount);
+  particles.setAttribute(
+    "position",
+    new THREE.BufferAttribute(particlePositions, 3).setUsage(
+      THREE.DynamicDrawUsage
+    )
+  );
+
+  // create the particle system
+  pointCloud = new THREE.Points(particles, pMaterial);
+  group.add(pointCloud);
+
+  const geometry = new THREE.BufferGeometry();
+
+  geometry.setAttribute(
+    "position",
+    new THREE.BufferAttribute(positions, 3).setUsage(THREE.DynamicDrawUsage)
+  );
+  geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(colors, 3).setUsage(THREE.DynamicDrawUsage)
+  );
+
+  geometry.computeBoundingSphere();
+
+  geometry.setDrawRange(10, 10);
+
+  const material = new THREE.LineBasicMaterial({
+    vertexColors: true,
+    blending: THREE.AdditiveBlending,
+    transparent: true,
+    color: 0x0abdc6,
+  });
+
+  linesMesh = new THREE.LineSegments(geometry, material);
+  group.add(linesMesh);
+
+  //
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  //
+
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+
+function animate() {
+  let vertexpos = 0;
+  let colorpos = 0;
+  let numConnected = 0;
+
+  for (let i = 0; i < particleCount; i++) particlesData[i].numConnections = 0;
+
+  for (let i = 0; i < particleCount; i++) {
+    // get the particle
+    const particleData = particlesData[i];
+
+    particlePositions[i * 3] += particleData.velocity.x;
+    particlePositions[i * 3 + 1] += particleData.velocity.y;
+    particlePositions[i * 3 + 2] += particleData.velocity.z;
+
+    if (
+      particlePositions[i * 3 + 1] < -rHalf ||
+      particlePositions[i * 3 + 1] > rHalf
+    )
+      particleData.velocity.y = -particleData.velocity.y;
+
+    if (particlePositions[i * 3] < -rHalf || particlePositions[i * 3] > rHalf)
+      particleData.velocity.x = -particleData.velocity.x;
+
+    if (
+      particlePositions[i * 3 + 2] < -rHalf ||
+      particlePositions[i * 3 + 2] > rHalf
+    )
+      particleData.velocity.z = -particleData.velocity.z;
+
+    if (
+      effectController.limitConnections &&
+      particleData.numConnections >= effectController.maxConnections
+    )
+      continue;
+
+    // Check collision
+    for (let j = i + 1; j < particleCount; j++) {
+      const particleDataB = particlesData[j];
+      if (
+        effectController.limitConnections &&
+        particleDataB.numConnections >= effectController.maxConnections
+      )
+        continue;
+
+      const dx = particlePositions[i * 3] - particlePositions[j * 3];
+      const dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
+      const dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
+      const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+      if (dist < effectController.minDistance) {
+        particleData.numConnections++;
+        particleDataB.numConnections++;
+
+        const alpha = 1.0 - dist / effectController.minDistance;
+
+        positions[vertexpos++] = particlePositions[i * 3];
+        positions[vertexpos++] = particlePositions[i * 3 + 1];
+        positions[vertexpos++] = particlePositions[i * 3 + 2];
+
+        positions[vertexpos++] = particlePositions[j * 3];
+        positions[vertexpos++] = particlePositions[j * 3 + 1];
+        positions[vertexpos++] = particlePositions[j * 3 + 2];
+
+        colors[colorpos++] = alpha;
+        colors[colorpos++] = alpha;
+        colors[colorpos++] = alpha;
+
+        colors[colorpos++] = alpha;
+        colors[colorpos++] = alpha;
+        colors[colorpos++] = alpha;
+
+        numConnected++;
+      }
+    }
+  }
+
+  linesMesh.geometry.setDrawRange(0, numConnected * 2);
+  linesMesh.geometry.attributes.position.needsUpdate = true;
+  linesMesh.geometry.attributes.color.needsUpdate = true;
+
+  pointCloud.geometry.attributes.position.needsUpdate = true;
+
+  requestAnimationFrame(animate);
+
+  render();
+  
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function handleMouseMove(event) {
+  targetX = event.clientX * 0.001;
+  targetY = event.clientY * 0.0015;
+}
+
+function render() {
+  // Use lerp to smoothly transition from the current rotation to the target rotation
+  group.rotation.y += (targetX - group.rotation.y) * 0.020;
+  group.rotation.x += (targetY - group.rotation.x) * 0.020;
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  renderer.render(scene, camera);
+}
+
   return (
-    <>
     <div ref={refContainer}></div>
-    </>
+
   );
 }
+
+
+
+export default MyThree;
+
+
+
 
